3ff8d7593e7bc4659908ff266bd8c7fdfcb5f982
Danny Murphy <murphy@danny-murphy.com>
2022-02-02 12:49:18 +1030

    switch from cookies to local storage

--- js/sidebars.js
+++ js/sidebars.js
@@ -9,36 +9,22 @@
 
 
 
+const sidebarInfo = localStorage.getItem("sidebar");
 
-/**
- * Get the value of a cookie
- * Source: https://gist.github.com/wpsmith/6cf23551dd140fb72ae7
- * @param  {String} name  The name of the cookie
- * @return {String}       The cookie value
- */
- function getCookie (name) {
-	let value = `; ${document.cookie}`;
-	let parts = value.split(`; ${name}=`);
-	if (parts.length === 2) return parts.pop().split(';').shift();
-}
-
-let sidebar = getCookie('sidebar');
-if (sidebar === 'collapsed') {
+if (sidebarInfo === 'collapsed') {
   document.body.classList.add("collapsed");  
 }
 
 function navToggle() {
-
-  let currentSidebar = getCookie('sidebar');
-
+  let currentSidebar = localStorage.getItem("sidebar");
+  
   if (currentSidebar === 'collapsed') {
     document.body.classList.remove("collapsed");
-    document.cookie = 'sidebar=collapsed; path=/; max-age=0;';
+    localStorage.setItem("sidebar", "uncollapsed");
   } else {
     document.body.classList.add("collapsed");
-    document.cookie = 'sidebar=collapsed;';
+    localStorage.setItem("sidebar", "collapsed");
   }
-
 }
 
 
